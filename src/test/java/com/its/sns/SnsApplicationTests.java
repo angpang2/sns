9b371ca6443a2c80c6a1ba8992dcc622bf1b1517
angpang2 <whtjdcks1111@naver.com>
2022-12-21 11:45:30 +0900

    프로젝트 기본설정

---
+++ src/test/java/com/its/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
